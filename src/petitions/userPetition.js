7ba816c97a31e97e418c0e89f2cf7cc85c90332f
Tatiana Sanabria Pedraza <tatianasanabr@Invitados-MacBook-Pro.local>
2022-12-05 20:57:19 -0500

    Agrega vista productos inicial

--- src/petitions/userPetition.js
+++ src/petitions/userPetition.js
@@ -2,8 +2,24 @@ import axios from "axios";
 
 const API = "http://localhost:8080/";
 
+//Autenticación de usuario
 const postUser = (userEmail, userPassword) => {
     return axios.post(API + "login", { email: userEmail, password: userPassword})
 }
 
-export { postUser }
+//Usuario alojado en el session storage
+const getUser = () => {
+    return JSON.parse(sessionStorage.getItem("user"))
+}
+
+//Token de cada usuario
+const token = () => {
+    return getUser().accessToken
+}
+
+//Petición para traer lista de productos
+const getProduct = () => {
+    return axios.get(API + "products", { Authorization: "Bearer" + token()})
+}
+
+export { postUser, getUser, token, getProduct }
